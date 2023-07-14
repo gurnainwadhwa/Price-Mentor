ff458d705c5f791853cd13033a87f641757b7563
gurnainwadhwa <gurnainwadhwa29@gmail.com>
2023-07-14 10:00:56 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -87,8 +87,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
-    // const response = await fetch('https://price-mentor-by-gurnain.onrender.com/', {
+    // const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://price-mentor-by-gurnain.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
